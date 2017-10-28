18514624ed840215a8f604270ddaa5e71c33f1bd
senthilbalajiganesan <g.senthilbalaji@gmail.com>
2017-10-28 21:27:32 +0530

    Update FetchApi.js

--- src/FetchApi.js
+++ src/FetchApi.js
@@ -1,4 +1,4 @@
-const ROOT_URL = `http://139.59.47.243:3090/`;
+const ROOT_URL = `http://139.59.45.243:3090/`;
 // const ROOT_URL = `http://localhost:3090/`
 export const LOGIN_USER = `${ROOT_URL}signin`;
 export const SIGN_UP_USER = `${ROOT_URL}signup`;
